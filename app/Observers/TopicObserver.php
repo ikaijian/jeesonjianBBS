2c4752d0324e0a5b9d1d11523100674cb8cb3f4e
rname <1144285537@qq.com>
2018-12-01 15:42:00 +0800

    add queens

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -28,13 +29,24 @@ class TopicObserver
 //       生成话题摘录  make_excerpt()是自定义的辅助方法
         $topic->excerpt = make_excerpt($topic->body);
 
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
-
+//        $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
 
     }
 
+    /**
+     * 模型监控器的 saved()方法对应 Eloquent 的 saved 事件，此事件发生在创建和编辑时、数据入库以后
+     * @param Topic $topic
+     */
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+//            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //将 Slug 翻译的调用修改为队列执行的方式
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
+
+        }
+    }
 
 }
