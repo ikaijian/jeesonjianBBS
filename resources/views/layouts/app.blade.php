063e84f607abf4fc93c3954a444265380a52b2ba
rname <1144285537@qq.com>
2018-12-07 12:53:49 +0800

    网站设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
 
     <!-- CSRF Token -->
     <meta name="crsf-token" content="{{csrf_token()}}">
-    <title>@yield('title','jeesonBBS')-BBS</title>
-    <meta name="description" content="@yield('description', '爱好者社区')" />
+    <title>@yield('title','jeesonBBS')-{{ setting('site_name', 'jianBBS论坛') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description', 'jianBBS论坛 爱好者社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'jian论坛,社区,论坛,开发者论坛'))" />
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
